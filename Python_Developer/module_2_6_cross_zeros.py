5887a090f77d5441cc1e707c7a698af64958dde7
yurysinev <yuraseg@gmail.com>
2024-09-23 17:40:34 +0300

    Crosses & Zeros

---
+++ Python_Developer/module_2_6_cross_zeros.py
@@ -0,0 +1,62 @@
+# повторяю за видеозаписью:
+def check_winner():
+    if turn > 4:  # проверка раньше пятого хода бессмысленна
+        for w in ["X", "0"]:  # мое решение, чтобы не дублировать всю эту писанину
+            if area[0][0] == w and area[0][1] == w and area[0][2] == w:
+                return w
+            if area[1][0] == w and area[1][1] == w and area[1][2] == w:
+                return w
+            if area[2][0] == w and area[2][1] == w and area[2][2] == w:
+                return w
+            if area[0][0] == w and area[1][0] == w and area[2][0] == w:
+                return w
+            if area[1][0] == w and area[1][1] == w and area[1][2] == w:
+                return w
+            if area[2][0] == w and area[2][1] == w and area[2][2] == w:
+                return w
+            if area[0][0] == w and area[1][1] == w and area[2][2] == w:
+                return w
+            if area[2][0] == w and area[1][1] == w and area[2][0] == w:
+                return w
+        return "*"
+    else:
+        return "*"
+
+
+def draw_area():
+    for i in area:
+        print(*i)
+    print()
+
+
+area = [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']]
+print("Добро пожаловать в крестики-нолики")
+print("----------------------------------")
+draw_area()
+for turn in range(1, 10):
+    print(f"Ход: {turn}")
+    if turn % 2 == 0:
+        turn_char = '0'
+        print("Ходят нолики")
+    else:
+        turn_char = 'X'
+        print("Ходят крестики")
+    row = int(input("Введите номер строки (1,2,3) ")) - 1  # запрашиваем ввод
+    column = int(input("Введите номер столбца (1,2,3) ")) - 1
+    if area[row][column] == "*":
+        area[row][column] = turn_char
+    else:
+        print("Ячейка уже занята. Вы пропускаете ход.")
+        draw_area()
+        continue
+
+    draw_area()
+
+    if check_winner() == "X":
+        print("Победа крестиков")
+        break
+    if check_winner() == "0":
+        print("Победа ноликов")
+        break
+    if check_winner() in ["*", "X", "0"] and turn == 9:
+        print("Ничья")
