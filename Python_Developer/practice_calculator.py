13adfd2e4d8566bc40d4bf809369e6a8e4bdee1f
yurysinev <yuraseg@gmail.com>
2024-09-30 00:12:01 +0300

    Tkinker. Calculator

---
+++ Python_Developer/practice_calculator.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk()  # весь код - между этой строкой и window.mainloop()
+window.title("Калькулятор")  # задаем имя
+window.geometry("350x350")
+window.resizable(False, False)
+
+window.mainloop()
