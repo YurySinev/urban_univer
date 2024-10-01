1e36974c425f844155246b6f261007108661af1c
yurysinev <yuraseg@gmail.com>
2024-10-01 16:34:21 +0300

    Zero Division by High Math Rule

---
+++ Python_Developer/Module_04/true_math.py
@@ -0,0 +1,15 @@
+from math import inf
+
+
+def divine(x, y):
+    if y == 0:
+        return inf
+    else:
+        return x / y
+
+if __name__ == '__main__':
+    print(divine(57, 3))
+    print(divine(63, 0))
+    print(divine(263548, 8739))
+
+
