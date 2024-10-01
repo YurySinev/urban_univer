43a213b6838c6ee215b715a3e23550a1b547f630
yurysinev <yuraseg@gmail.com>
2024-10-01 16:36:09 +0300

    Modules

---
+++ Python_Developer/Module_04/module_4_1.py
@@ -0,0 +1,11 @@
+from true_math import divine as true_div
+from fake_math import divine as fake_div
+
+result1 = fake_div(69, 3)
+result2 = fake_div(3, 0)
+result3 = true_div(49, 7)
+result4 = true_div(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
