ca61a4b5729e7be8b20c450d4dfa107f8c5922e8
Yury Sinev <yuraseg@gmail.com>
2024-10-08 00:42:21 +0300

    Zero Division by High Math Rule

--- Python_Developer/hwork2.py
+++
@@ -1,7 +0,0 @@
-# Практическое задание по теме "Переменные".
-
-hwork_count = 12
-total_hours = 1.5
-course = "Python"
-average_hwork_time = total_hours / hwork_count
-print(f"Курс: {course}, всего задач: {hwork_count}, затрачено часов: {total_hours}, среднее время выполнения {average_hwork_time} часа")
