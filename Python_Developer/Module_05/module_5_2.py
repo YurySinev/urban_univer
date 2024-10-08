a0395f7742b9bc283a7f468584bb69d43bf2b2a5
yurysinev <yuraseg@gmail.com>
2024-10-08 14:29:27 +0300

    Class House w len & str

---
+++ Python_Developer/Module_05/module_5_2.py
@@ -0,0 +1,43 @@
+# Домашняя работа по уроку "Специальные методы классов"
+#
+# Задача "Магические здания":
+# Необходимо дополнить класс House следующими специальными методами:
+# __len__(self) - должен возвращать кол-во этажей здания self.number_of_floors.
+# __str__(self) - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>".
+
+
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
+
+# Вывод на консоль:
+# Название: ЖК Эльбрус, кол-во этажей: 10
+# Название: ЖК Акация, кол-во этажей: 20
+# 10
+# 20
