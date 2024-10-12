dcbc749a6ce270a0636cfa213003813acb0f7348
yurysinev <yuraseg@gmail.com>
2024-10-12 20:08:56 +0300

    Just Practice

--- Python_Developer/Module_05/test.py
+++ Python_Developer/Module_05/test.py
@@ -28,3 +28,14 @@ data = [4, 7, 2, 4, 5, 8, 9, 1, 0, 5, 2]
 dic = {'name': 'Yury', 'age': 63, 'gender': 'male', 'height': 182, 'weight': 81}
 user = Example(10, 11,12)
 ex = Example('data', second=25, third=3.14)
+symbols = [chr(i) for i in range(33, 48)]
+caps = [chr(i) for i in range(65,91)]
+nums = [i for i in range(0,10)]
+letters = [chr(i) for i in range(97, 123)]
+print("symbols: ", symbols)
+print(caps)
+print(nums)
+print(letters)
+password = '$%&'
+print(list(password))
+print(any(list(password)) in symbols)
