d7f60ba052bedd75388e644e5cf05c83f1f2669b
Yury Sinev <yuraseg@gmail.com>
2024-10-12 11:28:49 +0300

    User Authorization & class Database Practice. Add: walrus, while, choice

--- Python_Developer/Module_05/class_practice1.py
+++ Python_Developer/Module_05/class_practice1.py
@@ -5,11 +5,12 @@ class User:
 
     def __init__(self, username, password, password_confirm):
         self.username = username
-        if password == password_confirm:
-            self.password = password
-        else:
-            print("Пароли не совпадают. Повторите ввод.")
-            return self.__init__(username, input("Введите пароль: "), input("Введите пароль еще раз: "))
+        self.password = password
+        # if password == password_confirm:
+        #     self.password = password
+        # else:
+        #     print("Пароли не совпадают. Повторите ввод.")
+        #     return self.__init__(username, input("Введите пароль: "), input("Введите пароль еще раз: "))
         self.email = None
 
     def __str__(self):
@@ -29,7 +30,10 @@ class Database():
 
 if __name__ == '__main__':
     database = Database()
-    user = User(input("Введите логин: "), input("Введите пароль: "), input("Введите пароль еще раз: "))
-    print(user)
-    database.add_user(user.username,user.password)
-    print(database.data)
+    while True:
+        choice = input("Приветствуем Вас! Выберите действие: \n1. Вход \n2. Регистрация")
+        user = User(input("Введите логин: "), pass1 := input("Введите пароль: "), pass2 := input("Введите пароль еще раз: "))
+        if pass1 != pass2:
+            exit()
+        database.add_user(user.username,user.password)
+        print(database.data)
