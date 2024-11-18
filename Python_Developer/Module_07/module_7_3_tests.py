228fb27c7f061e7197817c819240cbf670999f59
yurysinev <yuraseg@gmail.com>
2024-11-18 13:55:56 +0300

    Module 7. Operator with. Method find repaired

--- Python_Developer/Module_07/module_7_3_tests.py
+++ Python_Developer/Module_07/module_7_3_tests.py
@@ -7,4 +7,5 @@ wf = WordsFinder('file1.txt, file2.txt', ['file3.txt', ('file4.txt', 'file5.txt,
 finder2 = WordsFinder('test_file.txt', 'sample.txt')
 print(finder2.get_all_words())  # Все слова
 print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.find('строка'))  # 7 слово по счёту
 print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
