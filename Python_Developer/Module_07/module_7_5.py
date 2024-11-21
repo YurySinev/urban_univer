ec9ba9a18594765e0d97580d5c2e29ee18da8141
Yury Sinev <yuraseg@gmail.com>
2024-11-21 14:47:17 +0300

    Rename file os.py -> module_os.py

--- Python_Developer/Module_07/module_7_5.py
+++ Python_Developer/Module_07/module_7_5.py
@@ -11,7 +11,7 @@ for root, dirs, files in os.walk(directory):
         filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.path.getsize(filepath)
-        parent_dir = os.path.basename(os.path.dirname(filepath))
+        parent_dir = os.path.dirname(filepath)
         # print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, '
         #       f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
         table_data.append([file, filepath, filesize, formatted_time, parent_dir])
