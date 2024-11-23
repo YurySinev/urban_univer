975dc694f5dc924d31c4c6f68b25e5fa228d55c8
yurysinev <yuraseg@gmail.com>
2024-11-24 02:39:33 +0300

    Module Tkinter. Explorer

---
+++ Python_Developer/Module_07/explorer/explorer.py
@@ -0,0 +1,29 @@
+import tkinter
+import tkinter.ttk
+import os
+from tkinter import filedialog
+
+
+def file_select():
+    filename = filedialog.askopenfile(initialdir='.', title='Выберите файл',
+                                      filetypes=(('Текстовый файл', '.txt'), ('Все файлы', '*')))
+    text['text'] = text['text'] + ' ' + filename
+    os.startfile(filename)
+
+
+window = tkinter.Tk()
+window.title('Проводник')
+window.geometry('450x150')
+window.configure(bg='black')
+window.resizable(False, False)
+
+text = tkinter.Label(window, text='Файл: ', height=5, width=50,
+                     background='silver', foreground='blue')
+text.grid(column=1, row=1)
+
+button_select = tkinter.Button(window, text='Выбрать файл',
+                               width=20, height=3, background='silver', foreground='blue',
+                               command=file_select)
+button_select.grid(column=1, row=2, pady=1)
+
+window.mainloop()
