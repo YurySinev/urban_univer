7a6c780b57472d743f61c894eec41bc378844d58
yurysinev <yuraseg@gmail.com>
2024-12-17 03:01:08 +0300

    Module 9. Class Iterator

--- Python_Developer/Module_09/module_9_5.py
+++ Python_Developer/Module_09/module_9_5.py
@@ -1,69 +1,81 @@
 # Домашнее задание по теме "Итераторы"
 
-# Цель: освоить механизмы работы итераторов и описания методов __next__ и __iter__.
-# Закрепить навык создания и выбрасывания исключений.
-
-# Задача "Range - это просто":
-# Создайте пользовательский класс исключения StepValueError, который наследуется от ValueError.
+# Задача "Range - это просто"
 class StepValueError(ValueError):
-    def __init__(self, message = "Шаг указан неверно"):
+    def __init__(self, message="Шаг указан неверно"):
         self.message = message
-    # Наследования достаточно, класс оставьте пустым при помощи оператора pass.
-    # pass
 
-# Создайте класс Iterator, который обладает следующими свойствами:
+
 class Iterator:
     def __init__(self, start, stop, step=1):
         self.start = start  # start - целое число, с которого начинается итерация.
-        self.stop = stop    # stop - целое число, на котором заканчивается итерация.
-        if step == 0:       # проверяется step на равенство 0
+        self.stop = stop  # stop - целое число, на котором заканчивается итерация.
+        if step == 0:  # проверяется step на равенство 0
             raise StepValueError('Шаг не может быть равен 0')
         else:
-            self.step = step    # step - шаг, с которым совершается итерация.
-        self.pointer = start    # pointer - указывает на текущее число в итерации (изначально start)
+            self.step = step  # step - шаг, с которым совершается итерация.
+        self.pointer = start  # pointer - указывает на текущее число в итерации (изначально start)
 
-# Методы:
+    # Методы:
 
-    def __iter__(self):  # метод, сбрасывающий значение pointer на start и возвращающий сам объект итератора.
-        pass
-    def __next__(self): # метод, увеличивающий атрибут pointer на step. В зависимости от знака атрибута step итерация
-        # завершится либо когда pointer станет больше stop, либо меньше stop. Учтите это при описании метода.
-        pass
+    def __iter__(self):  # сбрасывает значение pointer на start и возвращает сам объект итератора.
+        self.pointer = self.pointer
+        return self
+
+    def __next__(self):
+        def moving_up():    # для движение вверх от start к stop
+            if self.pointer > self.stop:
+                raise StopIteration
+            else:
+                current_value = self.pointer
+                self.pointer += self.step
+                return current_value
+
+        def moving_down(): # для движения вниз от start к stop
+            if self.pointer < self.stop:
+                raise StopIteration
+            else:
+                current_value = self.pointer
+                self.pointer += self.step
+                return current_value
+
+        if self.start < self.stop and self.step > 0: # при соблюдении этих условий
+            return moving_up() # движемся вверх
+        elif self.start > self.stop and self.step < 0: # при этих условиях
+            return moving_down() # движемся вниз
+        else:
+            raise StopIteration # при иных условиях никуда не движемся
 
 
-# Пример результата выполнения программы:
+if __name__ == '__main__':
+    # Пример результата выполнения программы:
+    try:
+        iter1 = Iterator(100, 200, 0)
+        for i in iter1:
+            print(i, end=' ')
+    except StepValueError as exc:
+        print('Шаг указан неверно')
 
-try:
-    iter1 = Iterator(100, 200, 0)
-    for i in iter1:
+    iter2 = Iterator(-5, 1)
+    iter3 = Iterator(6, 15, 2)
+    iter4 = Iterator(5, 1, -1)
+    iter5 = Iterator(10, 1)
+
+    for i in iter2:
         print(i, end=' ')
-except StepValueError as exc:
-    print('Шаг указан неверно')
-
-iter2 = Iterator(-5, 1)
-iter3 = Iterator(6, 15, 2)
-iter4 = Iterator(5, 1, -1)
-iter5 = Iterator(10, 1)
-
-
-# for i in iter2:
-#     print(i, end=' ')
-#     print()
-# for i in iter3:
-#     print(i, end=' ')
-#     print()
-# for i in iter4:
-#     print(i, end=' ')
-#     print()
-# for i in iter5:
-#     print(i, end=' ')
-#     print()
+    print()
+    for i in iter3:
+        print(i, end=' ')
+    print()
+    for i in iter4:
+        print(i, end=' ')
+    print()
+    for i in iter5:
+        print(i, end=' ')
+    print()
 
 # Вывод на консоль:
 # Шаг указан неверно
 # -5 -4 -3 -2 -1 0 1
 # 6 8 10 12 14
 # 5 4 3 2 1
-
-# Примечания:
-# Особое внимание уделите методу __next__ и условиям в нём.
