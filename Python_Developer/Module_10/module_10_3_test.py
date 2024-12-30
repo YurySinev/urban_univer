ed918bdcafe8335899297af1505b8bef5ce565cc
yurysinev <yuraseg@gmail.com>
2024-12-30 20:41:03 +0300

    Module 10. Flow Blocks

---
+++ Python_Developer/Module_10/module_10_3_test.py
@@ -0,0 +1,14 @@
+import random
+import threading
+from module_10_3 import Bank
+
+# print(random.randint(50, 500))
+bk = Bank()
+th1 = threading.Thread(target=bk.deposit)
+th2 = threading.Thread(target=bk.take)
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+print()
+print(bk)
